f38863c431274666687bde6dfd2dd724852de269
Iulian Igas <41596757+iulianigas@users.noreply.github.com>
2021-10-12 00:15:42 +0300

    Added next 20 exercices

---
+++ ex11.rb
@@ -0,0 +1,9 @@
+print "How old are you? "
+age = gets.chomp
+print "How tall are you(centimeters)? "
+height = gets.chomp
+print "How much do you weigh(killograms)? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
